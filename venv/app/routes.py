5419f27cabc1a94ddc3ed79d5d13c64df6e26ad3
Rockim St. Bernard <rockim313@hotmail.com>
2020-04-05 23:52:06 -0400

    flask template files

--- venv/app/routes.py
+++ venv/app/routes.py
@@ -1,7 +1,19 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Rockim'}
+    posts= [
+        {
+            'author': {'username':'John'},
+            'body':'Beatiful day in Portland!'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'Beatiful day in Portland!'
+        }
+    ]
+    return  render_template('index.html',title='Home', user=user, posts=posts)
