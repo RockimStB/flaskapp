e7f08cf000de67f1eb612f3c7ff0a30edb83dc61
Rockim St. Bernard <rockim313@hotmail.com>
2020-04-24 21:55:29 -0400

    app confi

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+# ... add more variables here as needed
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
